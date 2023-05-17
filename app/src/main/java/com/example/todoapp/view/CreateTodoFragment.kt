746a7a579f9fb52b72a320b804a3929bed3badbc
Kecambah123 <119327983+Kecambah123@users.noreply.github.com>
2023-05-17 11:15:56 +0700

    progress 1

---
+++ app/src/main/java/com/example/todoapp/view/CreateTodoFragment.kt
@@ -0,0 +1,66 @@
+package com.example.todoapp.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
+import com.example.todoapp.R
+import com.example.todoapp.model.Todo
+import com.example.todoapp.viewmodel.DetailTodoViewModel
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [CreateTodoFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class CreateTodoFragment : Fragment() {
+    private lateinit var viewModel: DetailTodoViewModel
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(DetailTodoViewModel::class.java)
+        val btnAdd = view.findViewById<Button>(R.id.btnAdd)
+        btnAdd.setOnClickListener {
+            val txtTitle = view.findViewById<EditText>(R.id.txtTitle)
+            val txtNotes = view.findViewById<EditText>(R.id.txtNotes)
+            var todo = Todo(txtTitle.text.toString(), txtNotes.text.toString())
+            val list = listOf(todo)
+            viewModel.addTodo(list)
+            Toast.makeText(view.context, "Data added", Toast.LENGTH_LONG).show()
+            Navigation.findNavController(it).popBackStack()
+        }
+    }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_todo, container, false)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment CreateTodoFragment.
+         */
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            CreateTodoFragment().apply {
+                arguments = Bundle().apply {
+
+                }
+            }
+    }
+}
---
+++ app/src/main/java/com/example/todoapp/view/TodoListFragment.kt
@@ -0,0 +1,81 @@
+package com.example.todoapp.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.todoapp.R
+import com.example.todoapp.viewmodel.ListTodoViewModel
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [TodoListFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class TodoListFragment : Fragment() {
+    private lateinit var viewModel: ListTodoViewModel
+    private val todoListAdapter  = TodoListAdapter(arrayListOf(),{ item -> viewModel.clearTask(item) })
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this).get(ListTodoViewModel::class.java)
+        viewModel.refresh()
+        var recViewTodo = view.findViewById<RecyclerView>(R.id.recViewTodo)
+        recViewTodo.layoutManager = LinearLayoutManager(context)
+        recViewTodo.adapter = todoListAdapter
+
+        val fabAddTodo = view.findViewById<FloatingActionButton>(R.id.fabAddTodo)
+        fabAddTodo.setOnClickListener {
+            val action = TodoListFragmentDirections.actionCreateTodo()
+            Navigation.findNavController(it).navigate(action)
+        }
+        observeViewModel()
+    }
+
+    fun observeViewModel() {
+        viewModel.todoLD.observe(viewLifecycleOwner, Observer {
+            todoListAdapter.updateTodoList(it)
+            var txtEmpty = view?.findViewById<TextView>(R.id.txtEmpty)
+            if(it.isEmpty()) {
+                txtEmpty?.visibility = View.VISIBLE
+            } else {
+                txtEmpty?.visibility = View.GONE
+            }
+        })
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_todo_list, container, false)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment TodoListFragment.
+         */
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            TodoListFragment().apply {
+                arguments = Bundle().apply {
+
+                }
+            }
+    }
+}
